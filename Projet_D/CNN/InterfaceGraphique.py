7ab1936140c1e68d6b6500d5b771a68cf640c4d0
Corty Suss <Corty.Suss@phelma.grenoble-inp.fr>
2018-05-27 14:45:46 +0200

    interface graphique

--- Projet_D/CNN/InterfaceGraphique.py
+++ Projet_D/CNN/InterfaceGraphique.py
@@ -7,6 +7,7 @@ import time
 import Perceptron as p
 import imageReader
 import pickle
+import inmageTest
 
 def unpickle(file):
     import pickle
@@ -50,21 +51,18 @@ def switch_class(number):
     return switcher.get(number)
 
 
-def testWithInterface(inFile, test_images, netScratch, netPyTorch):
+def testWithInterface(inFile, test_images, netScratch):
     fenetre = Tk()
     canvas = Canvas(fenetre, width=1350, height=800, background="black")
     canvas.pack()
 
     for i in range(10):
         txt = canvas.create_text(75, i*65 +110, text=switch_class(i), font="Arial 25", fill="white")
-        txt = canvas.create_text(1220, i*65 +110, text=switch_class(i), font="Arial 25", fill="white")
 
     txt = canvas.create_text(200, 40, text="Scratch", font="Arial 40 bold", fill="yellow")
-    txt = canvas.create_text(1100, 40, text="PyTorch", font="Arial 40 bold", fill="yellow")
 
     for i in range(10):
         a2 = canvas.create_rectangle(130,i*65 + (65/2 - 5) +60,325, i*65 +3*65/2-25 + 60, outline = 'white')
-        a2 = canvas.create_rectangle(970,i*65 + (65/2 - 5) +60,1165, i*65 +3*65/2-25 + 60, outline = 'white')
 
     for training in range(len(test_images[0])):
         training = np.random.randint(len(test_images[0]))
@@ -75,46 +73,38 @@ def testWithInterface(inFile, test_images, netScratch, netPyTorch):
         canvas.update()
         input()
         resultatScratch = netScratch.propagation(test_images[0][training])
-        resultatPyTorch = netScratch.propagation(test_images[0][training])
         for j in range(10):
             remplissage = canvas.create_rectangle(130,j*65 + 60 + (65/2 - 5), 130+195*resultatScratch[j], j*65 + 60 +3*65/2-25, fill ='white', outline = 'white')
-            remplissage = canvas.create_rectangle(970,j*65 + 60 + (65/2 - 5), 970+195*resultatPyTorch[j], j*65 + 60 +3*65/2-25, fill ='white', outline = 'white')
             canvas.update()
         input()
         sScratch = np.argmax(resultatScratch)
-        sPyTorch = np.argmax(resultatPyTorch)
         if sScratch == np.argmax(test_images[1][training]):
             colorScratch = 'green'
         else :
             colorScratch = 'red'
-        if sPyTorch == np.argmax(test_images[1][training]):
-            colorPyTorch = 'green'
-        else :
-            colorPyTorch = 'red'
 
         sortieScratch = canvas.create_rectangle(130,sScratch*65 + 60 + (65/2 - 5), 130+195*resultatScratch[sScratch], sScratch*65 + 60 +3*65/2-25, fill =colorScratch, outline = 'white')
-        sortiePyTorch = canvas.create_rectangle(970,sPyTorch*65 + 60 + (65/2 - 5), 970+195*resultatPyTorch[sPyTorch], sPyTorch*65 + 60 +3*65/2-25, fill =colorPyTorch, outline = 'white')
         canvas.update()
         input()
 
         for i in range(10):
             a2 = canvas.create_rectangle(130,i*65 + (65/2 - 5) +60,325, i*65 +3*65/2-25 + 60, fill = 'black', outline = 'white')
-            a2 = canvas.create_rectangle(970,i*65 + (65/2 - 5) +60,1165, i*65 +3*65/2-25 + 60, fill = 'black' ,outline = 'white')
         canvas.delete(image)
         canvas.delete(label)
         canvas.delete(sortieScratch)
-        canvas.delete(sortiePyTorch)
 
     fenetre.mainloop()
 
 def imgSaveMnist(inFile, numImg):
     data = inmageTest.convert_matrix(inFile, 16 + 784 * numImg).astype('uint8')
+    data = np.fliplr(data)
     img = Image.fromarray(data)
     img = img.resize((28*20,28*20))
+    img = img.rotate(90)
     img.save('my.png')
 
 
-def interfaceMnist(Perceptron, inFile, cas, rangeTest, test_images, labelled_images, rangeLabel):
+def interfaceEMnist(Perceptron, inFile, cas, test_images):
     if cas == True :
         var = 25
     else :
@@ -123,22 +113,22 @@ def interfaceMnist(Perceptron, inFile, cas, rangeTest, test_images, labelled_ima
     canvas = Canvas(fenetre, width=1350, height=700, background="black")
     canvas.pack()
 
-    for i in range(10*65/var):
+    for i in range(int(10*65/var)):
         if(cas) :
-            txt = canvas.create_text(900, i*var +50*var/65, text=chr(i+97), font="Arial " + str(35*var/65), fill="white")
+            txt = canvas.create_text(900, i*var +50*var/65, text=chr(i+97), font="Arial " + str(int(35*var/65)), fill="white")
         else :
-            txt = canvas.create_text(900, i*var +50*var/65, text=str(i), font="Arial " + str(35*var/65), fill="white")
+            txt = canvas.create_text(900, i*var +50*var/65, text=str(i), font="Arial " + str(int(35*var/65)), fill="white")
         a2 = canvas.create_rectangle(930,i*var + (var/2 - 5*var/65),1125, i*var +3*var/2-25*var/65, outline = 'white')
 
 
-    for i in range(rangeTest):
-        i = np.random.randint(rangeTest)
+    for i in range(10000):
+        i = np.random.randint(10000)
         imgSaveMnist(inFile,i)
         new_img = PhotoImage(file = 'my.png')
         image = canvas.create_image(400,350, image = new_img)
         canvas.update()
         input()
-        resultat = percep.propagation(test_images[0][i])
+        resultat = Perceptron.propagation(test_images[0][i])
         for j in range(len(resultat)):
                 remplissage = canvas.create_rectangle(930,j*var + (var/2 - 5*var/65), 930+195*resultat[j], j*var +3*var/2-25*var/65, fill ='white', outline = 'white')
         canvas.update()
@@ -151,7 +141,7 @@ def interfaceMnist(Perceptron, inFile, cas, rangeTest, test_images, labelled_ima
         canvas.update()
         input()
         canvas.delete(sortie)
-        for i in range(10*65/var):
+        for i in range(int(10*65/var)):
             a2 = canvas.create_rectangle(930,i*var + (var/2 - 5*var/65),1125, i*var +3*var/2-25*var/65,fill = 'black', outline = 'white')
         canvas.delete(image)
         canvas.update()
@@ -162,8 +152,13 @@ def interfaceMnist(Perceptron, inFile, cas, rangeTest, test_images, labelled_ima
 
 #test_images = inmageTest.list_labelled_images('t10k-images-idx3-ubyte', 't10k-labels-idx1-ubyte', 10000, 0)
 #interfaceMnist(percep,'t10k-images-idx3-ubyte', True, 10000, test_images, labelled_images,60000)
+i=input()
+if(int(i)==1):
+##################### Letters test ##########################
+    test_images = imageReader.list_labelled_images2D('emnist-letters-test-images-idx3-ubyte', 'emnist-letters-test-labels-idx1-ubyte', 10000, 0, 'letters')
+    interfaceEMnist(pickle.load(open('Tests/MNIST/EMnist_CNN_batch4','rb'))[0][1],'emnist-letters-test-images-idx3-ubyte',True, test_images)
 
-
-
-test_images = get_data('cifar-10-batches-py/test_batch')
-testWithInterface('cifar-10-batches-py/test_batch', test_images, pickle.load(open('CIFAR10test', 'rb'))[0][1],pickle.load(open('CIFAR10test', 'rb'))[0][1])
+else :
+##################### Cifar Test ###########################
+    test_images = get_data('cifar-10-batches-py/test_batch')
+    testWithInterface('cifar-10-batches-py/test_batch', test_images, pickle.load(open('cifar-10-batches-py/CIFAR10_1batch5', 'rb'))[0][1])
